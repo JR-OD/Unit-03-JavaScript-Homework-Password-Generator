26dd4178fcea716eab5a320bc64e300e9860be4d
J.RoD <tradeadvocacy@yahoo.com>
2019-10-04 14:48:04 -0700

    updating js, finsh stylesheet, html and readme

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
+#Unit 03 JavaScript Homework: 
+
+
+## Password Generator
+
+This application generates a random password based on user-selected criteria, featuring dynamically updated HTML and CSS powered by your JavaScript code.
+
+The user can select the following password criteria:
+
+Length (between 8 and 128 characters range)
+
+Character type:
+    Special characters
+    Numeric characters
+    Lowercase characters
+    Uppercase characters
+
+The user has the option to click a button to copy the password to their clipboard.
+
+
+## User Story
+
+AS AN employee with access to sensitive data
+
+I WANT to randomly generate a password that meets certain criteria
+
+SO THAT I can create a strong password that provides greater security
+
+
+## Business Context
+
+For companies that handle large amounts of sensitive data, weak passwords can pose a real security threat. An application that can generate strong passwords quickly and effortlessly saves employees time and ensures secure access to data.
+
+
+##General Criteria
+
+GIVEN that a user needs a new, secure password
+
+WHEN prompted for password criteria
+
+THEN a password is generated
+
+
+## Method
+
+Applied:
+    Bootstrap CSS Framework; 
+    getElemt;
+    eventLisner;
+    variables;
+    prompts;
+    alerts;
+    functions;
+    for loops;
+
+## URL:
+
+
 # Unit-03-JavaScript-Homework-Password-Generator
 https://uclax.bootcampcontent.com/UCLA-Coding-Boot-Camp/UCLA-LA-FSF-PT-09-2019-U-C/tree/master/01-Class-Content/03-JavaScript/02-Homework
--- script.js
+++ script.js
@@ -1,49 +1,79 @@
-// variables: 
-var special = ("~,!,@,#,$,%,^,&,*,-,+,?,");
-var numeric = ("0,1,2,3,4,5,6,7,8,9");
-var lowercase = ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,y,z");
-var uppercase = ("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,X,Y,Z");
 var generate = document.getElementById("generate")
-var randomPassword = document.getElementById("password")
-
-// user imput:
-
+// var password = document.getElementById("password")
 
 generate.addEventListener("click",genesis)
 
-function genesis() {
+    function genesis() {
 
-    var size = prompt ("Please choose a pasword length between 8 and 128 characters:");
+        // variables: 
+        var special = ("~,!,@,#,$,%,^,&,*,-,+,?,");
+        var numeric = ("0,1,2,3,4,5,6,7,8,9");
+        var lowercase = ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,y,z");
+        var uppercase = ("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,X,Y,Z");
+        
+        // user imput:
 
-    var type =
-    confirm("Special characters") ? type += special: '';
-    confirm("Numeric characters") ? type += numeric: '';
-    confirm("lowercase characters") ? type += lowercase: '';
-    confirm("Uppercase characters") ? type += uppercase: '';
+        var size = prompt ("Please choose a pasword length between 8 and 128 characters:").value;
+        if(size < 8 || size > 128) 
+            alert("Minimum 8 and maximum 128 characters");
 
-    randomPassword.value = password(size, type);
-};
+        var type = [char1 += char2 += char3 += char4]
+        confirm("Special characters")
+            if (true) {
+                var char1 = special;
+            } else {''};
 
-console.log(password)
+        confirm("Numeric characters")
+            if (true){
+                var char2 = numeric;
+            }else {''};
+            
+        confirm("lowercase characters") 
+            if (true) {
+                var char3 = lowercase;
+            } else {''};
+            
+        confirm("Numeric characters") 
+            if (true) {
+                var char4 = uppercase;
+            } else {''};
+            
+        if (type = '')
+            alert("must choose at least one character type");
 
-// pasword generation
+        for(var i=0; i<size; i++) {
+            password = password + type.charArt(Math.floor(Math.random() * Math.floor(type.size - 1)));
+        }
 
-function password(size,type){
-    var psswrd = '';
+        // publishing result
 
-    for(var i=0; i<size; i++) {
-        psswrd += type.charAt(Math.floor(Math.random() * type.length));
+        document.getElementById("generate").value = password; 
     }
-    return psswrd;
-
-    console.log(psswrd)
-}
 
 // clipboard
 
-// function.clipboard() {
-//     document.getElementById("generate");
+function clipboard() {
+    document.getElementById("generate").Select();
 
-//     document.execCommand("Copy");
+    document.execCommand("Copy");
 
-//     alert(password copied); //
+    alert("password copied");
+}
+
+
+//     password.value = random(size, type);
+// console.log(password)
+
+// // password generation
+
+// function random(size,type){
+//     var psswrd = '';
+
+//     for(var i=0; i<size; i++) {
+//         psswrd += type.charAt(Math.floor(Math.random() * type.length));
+//     }
+//     return psswrd;
+
+//     console.log(psswrd)
+// }
+
