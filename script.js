e39dded72375501327646d1b831835c607a5b842
J.RoD <tradeadvocacy@yahoo.com>
2019-10-05 16:03:41 -0700

    cleaning script and adding Xcript to show multiple psedo altertive paths taken

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
-var generate = document.getElementById("generate")
-// var password = document.getElementById("password")
-
-generate.addEventListener("click",genesis)
-
-    function genesis() {
-
-        // variables: 
-        var special = ("~,!,@,#,$,%,^,&,*,-,+,?,");
-        var numeric = ("0,1,2,3,4,5,6,7,8,9");
-        var lowercase = ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,x,y,z");
-        var uppercase = ("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,X,Y,Z");
+function genesis() {
         
-        // user imput:
+    // variables: 
+    var special = ["~","!","@","#","$","%","^","&","*","-","+","?"]; 
+    var numeric = [0,1,2,3,4,5,6,7,8,9];
+    var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+    var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","X","Y","Z"];
+    var type = [];
+    var size = [];
+    
+    // user imput:
+    
+    function charType () {
 
-        var size = prompt ("Please choose a pasword length between 8 and 128 characters:").value;
-        if(size < 8 || size > 128) 
-            alert("Minimum 8 and maximum 128 characters");
+    specialChar = confirm("Special characters")
+        if (specialChar==true) {
+            type.push(special);
+        } else {''};
 
-        var type = [char1 += char2 += char3 += char4]
-        confirm("Special characters")
-            if (true) {
-                var char1 = special;
-            } else {''};
-
-        confirm("Numeric characters")
-            if (true){
-                var char2 = numeric;
-            }else {''};
-            
-        confirm("lowercase characters") 
-            if (true) {
-                var char3 = lowercase;
-            } else {''};
-            
-        confirm("Numeric characters") 
-            if (true) {
-                var char4 = uppercase;
-            } else {''};
-            
-        if (type = '')
+    numChar = confirm("Numeric characters")
+        if (numChar==true){
+            type.push(numeric);
+        }else {''};
+        
+    lowChar = confirm("lowercase characters") 
+        if (lowChar==true) {
+            type.push(lowercase);
+        } else {''};
+        
+    uppChar = confirm("Numeric characters") 
+        if (uppChar==true) {
+            type.push(uppercase);
+        } else {''};
+        
+    var type = []
+        if (type = ''){
             alert("must choose at least one character type");
-
-        for(var i=0; i<size; i++) {
-            password = password + type.charArt(Math.floor(Math.random() * Math.floor(type.size - 1)));
+            return;
         }
-
-        // publishing result
-
-        document.getElementById("generate").value = password; 
+    
+    var myJSON = JSON.stringify(type);
+    return type.sort().join('');
+    
     }
+        
+    console.log(charType)
+        
+    var size = prompt ("Please choose a pasword length between 8 and 128 characters:");
+    if(size < 8 || size > 128) {
+        alert("Minimum 8 and maximum 128 characters");
+        return;
+    }
+                
+
+    for(var i=0; i<size; i++) {
+        password = password + type.charArt(Math.floor(Math.random() * Math.floor(type.size - 1)));
+    }
+
+    // publishing result
+
+    document.getElementById("generate").value = password; 
+}
 
 // clipboard
 
@@ -59,21 +70,3 @@ function clipboard() {
 
     alert("password copied");
 }
-
-
-//     password.value = random(size, type);
-// console.log(password)
-
-// // password generation
-
-// function random(size,type){
-//     var psswrd = '';
-
-//     for(var i=0; i<size; i++) {
-//         psswrd += type.charAt(Math.floor(Math.random() * type.length));
-//     }
-//     return psswrd;
-
-//     console.log(psswrd)
-// }
-
